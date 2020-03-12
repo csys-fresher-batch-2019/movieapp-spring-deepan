b991151b2b41dca9b545e41e42c2481e224fb2bc
Deepanraj R <deepanraj151997@gmail.com>
2020-03-12 13:10:57 +0530

    updated

--- src/main/java/com/chainsys/movieapp/dao/impl/MovieListDAOImpl.java
+++ src/main/java/com/chainsys/movieapp/dao/impl/MovieListDAOImpl.java
@@ -11,12 +11,14 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
 
 import com.chainsys.movieapp.dao.MovieListDAO;
 import com.chainsys.movieapp.exception.DbException;
 import com.chainsys.movieapp.model.Movie;
 import com.chainsys.movieapp.util.DbConnection;
 
+@Repository
 public class MovieListDAOImpl implements MovieListDAO {
 
 	private static final Logger logger = LoggerFactory.getLogger(MovieListDAOImpl.class);
